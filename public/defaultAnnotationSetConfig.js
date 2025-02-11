0380fa745c022c91f1f92bd2781834e11c6d3668
Olleesch <olle.e.sch@gmail.com>
2025-02-11 01:18:59 +0100

    Add draft of interface for annotation sets

---
+++ public/defaultAnnotationSetConfig.js
@@ -0,0 +1,20 @@
+/**
+ * Default classConfig, if not supplied from imported annotation file.
+ * 
+ * The classConfig object specified in this file describes the default
+ * marker classes that can be used in the application. Each entry 
+ * describes a class's name, description, and color. 
+ * Order of classes matters for button order and sorting.
+ */
+const defaultAnnotationSetConfig = [
+    {
+        name: "Default",
+        description: "Default annotation set for manual annotation",
+        // color: "#346d2e"
+    },
+    {
+        name: "Test",
+        description: "Test annotation set for manual annotation",
+        // color: "#346d2e"
+    }
+];
