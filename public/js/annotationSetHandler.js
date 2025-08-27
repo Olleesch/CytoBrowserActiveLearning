5008b848670caa84ecf1177759e0a6214a7a0bc3
Olleesch <olle.e.sch@gmail.com>
2025-08-27 03:34:06 +0200

    Expand the remove annotation set function with two convenient parameters

--- public/js/annotationSetHandler.js
+++ public/js/annotationSetHandler.js
@@ -310,26 +310,43 @@ const annotationSetHandler = (function(){
      * @param {boolean} [transmit=true] Any collaborators should also be
      * told to remove the annotation set.
      */
-    function removeAnnotationSet(transmit = true) {
-        // Ask user if they are sure that they want to remove the annotation set (and all included annotations) first.
-        const title = "Are you sure you want to remove the currently selected annotation set and all its annotations?"
-        const choices = [{
-            label: "Yes",
-            click: () => {
-                if (isLockedAnnotationSet(_activeAnnotationSet.name)) {
-                    console.warn("Cannot remove a locked annotation set, skipping");
-                    return;
-                }
-                // Clear all annotations in the annotation set.
-                annotationHandler.clear(_activeAnnotationSet.name, transmit);
-                // Update annotation set config.
-                _annotationSetConfig = _annotationSetConfig.filter(annotationSet => {
-                    return annotationSet.name !== _activeAnnotationSet.name;
-                });
-                update(_annotationSetConfig, transmit);
+    function removeAnnotationSet(transmit = true, removedAnnotationSetID = null, force = false) {
+        function _removeFn(removedAnnotationSetName) {
+            if (isLockedAnnotationSet(removedAnnotationSetName)) {
+                console.warn("Cannot remove a locked annotation set, skipping");
+                return;
             }
-        }];
-        tmappUI.choice(title, null, choices);
+            // Clear all annotations in the annotation set.
+            annotationHandler.clear(removedAnnotationSetName, transmit);
+            // Update annotation set config.
+            _annotationSetConfig = _annotationSetConfig.filter(annotationSet => {
+                return annotationSet.name !== removedAnnotationSetName;
+            });
+            update(_annotationSetConfig, transmit);
+        }
+        
+        let removedAnnotationSetName;
+        if (removedAnnotationSetID) {
+            removedAnnotationSetName = getAnnotationSetFromID(removedAnnotationSetID).name;
+        }
+        else {
+            removedAnnotationSetName = _activeAnnotationSet.name;
+        }
+
+        if (force) {
+            _removeFn(removedAnnotationSetName);
+        }
+        else {
+            // Ask user if they are sure that they want to remove the annotation set (and all included annotations) first.
+            const title = "Are you sure you want to remove the currently selected annotation set and all its annotations?"
+            const choices = [{
+                label: "Yes",
+                click: () => {
+                    _removeFn(removedAnnotationSetName);
+                }
+            }];
+            tmappUI.choice(title, null, choices);
+        }
     }
 
     /**
