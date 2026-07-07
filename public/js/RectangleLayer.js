23fdfda9530d82d6e7fa0e5585813e3f9a5dd902
Olleesch <olle.e.sch@gmail.com>
2026-07-07 18:14:47 +0200

    Constrain rectangle vertex edits to stay axis-aligned

--- public/js/RectangleLayer.js
+++ public/js/RectangleLayer.js
@@ -8,8 +8,19 @@ class RectangleLayer extends RegionLayer {
         return annotation.type === "rectangle";
     }
 
-    // TODO: constrain to stay axis-aligned
+    // Axis-constrained rectangle modification. Dragging a corner keeps the 
+    // shape axis-aligned by leaving the diagonally opposite corner fixed and 
+    // recomputing the other two corners from it and the new position.
     _applyVertexDrag(points, index, newPos) {
+        const fixed = points[(index + 2) % 4];
+        [(index + 1) % 4, (index + 3) % 4].forEach(j => {
+            if (points[j].x === points[index].x) {
+                Object.assign(points[j], {x: newPos.x, y: fixed.y});
+            }
+            else {
+                Object.assign(points[j], {x: fixed.x, y: newPos.y});
+            }
+        });
         Object.assign(points[index], newPos);
     }
 }
