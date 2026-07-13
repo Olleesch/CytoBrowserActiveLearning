0859c4e4ee30868a305bcdc96c7256517a116fad
Olleesch <olle.e.sch@gmail.com>
2026-07-14 00:25:50 +0200

    Bugfix: Forward press/drag/release events when they start on an inactive layer's annotation

--- public/js/RegionLayer.js
+++ public/js/RegionLayer.js
@@ -270,7 +270,10 @@ class RegionLayer extends OverlayLayer {
                 }
             },
             pressHandler: (event) => {
-                if (layerHandler.topLayer() !== this) return;
+                if (layerHandler.topLayer() !== this) {
+                    forwardToViewportHandler(event, "pressHandler");
+                    return;
+                }
 
                 tmapp.setCursorStatus({held: true});
                 mouse_pos = new OpenSeadragon.Point(event.originalEvent.offsetX,event.originalEvent.offsetY);
@@ -279,13 +282,19 @@ class RegionLayer extends OverlayLayer {
                 this.#currentMouseUpdateFun=updateMousePos;
             },
             releaseHandler: (event) => {
-                if (layerHandler.topLayer() !== this) return;
+                if (layerHandler.topLayer() !== this) {
+                    forwardToViewportHandler(event, "releaseHandler");
+                    return;
+                }
 
                 tmapp.setCursorStatus({held: false});
                 this.#currentMouseUpdateFun=null;
             },
             dragHandler: (event) => {
-                if (layerHandler.topLayer() !== this) return;
+                if (layerHandler.topLayer() !== this) {
+                    forwardToViewportHandler(event, "dragHandler");
+                    return;
+                }
 
                 regionEditor.stopEditingRegion();
                 mouse_pos = new OpenSeadragon.Point(event.originalEvent.offsetX,event.originalEvent.offsetY);
